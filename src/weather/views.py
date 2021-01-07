b54925bf7598f3d95139563dcffc5d5c8e44af0e
Emre <emrekurtt1655@gmail.com>
2021-01-07 10:16:37 +0100

    add form

--- src/weather/views.py
+++ src/weather/views.py
@@ -2,16 +2,29 @@ from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from .models import City
+from .forms import CityForm
 
 def index(request):
+    form = CityForm()
+    cities = City.objects.all()
     url = config('BASE_URL')
-    city = "Berlin"
-    r = requests.get(url.format(city))
-    data = r.json()
+    city_data = []
+    for city in cities:
+        print(city)
+        r = requests.get(url.format(city))
+        data = r.json()
+        weather_data = {
+            "city" : city.name,
+            'temp' : data["main"]["temp"],
+            'description' : data["weather"][0]["description"],
+            'icon' : data["weather"][0]["icon"],
+        }
+        city_data.append(weather_data)
+    
     context = {
-        "city" : city,
-        'temp' : data["main"]["temp"],
-        'description' : data["weather"][0]["description"],
-        'icon' : data["weather"][0]["icon"],
+        "city_data" : city_data,
+        "form" : form
     }
+    
     return render(request, "weather/index.html", context)
