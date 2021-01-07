34b46ad80bdd89bd493409c15fac142d380ab4ff
Emre <emrekurtt1655@gmail.com>
2021-01-07 09:26:20 +0100

    add requests

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
 
 def index(request):
-    return render(request, "weather/index.html")
+    url = config('BASE_URL')
+    city = "Berlin"
+    r = requests.get(url.format(city))
+    data = r.json()
+    context = {
+        "city" : city,
+        'temp' : data["main"]["temp"],
+        'description' : data["weather"][0]["description"],
+        'icon' : data["weather"][0]["icon"],
+    }
+    return render(request, "weather/index.html", context)
